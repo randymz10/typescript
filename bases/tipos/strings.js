b3b12f6d6f65ba3db4a567fb5845b21792cdfeb5
randymz10 <randymz10@gmail.com>
2023-03-13 21:16:38 -0300

    Add new features

---
+++ bases/tipos/strings.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    const batman = 'Batman';
+    const linternaVerde = "Linterna";
+    const volvanNegro = `Héroe: Volvan Negro`;
+    console.log(` I'm ${batman} `);
+    console.log(batman.toUpperCase());
+})();
