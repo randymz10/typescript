108efabd7e09394c925ec73a8ec04d44e1a57616
randymz10 <randymz10@gmail.com>
2023-04-01 02:20:14 -0300

    Added new files

--- bases/app.js
+++ bases/app.js
@@ -1,5 +1,35 @@
 "use strict";
 (() => {
-    const a = 10;
-    console.log(a);
+    // Tipos
+    const batman = 'Bruce';
+    const superman = 'Clark';
+    const existe = false;
+    // Tuplas
+    const parejaHeroes = [batman, superman];
+    const villano = ['Lex Lutor', 5, true];
+    // Arreglos
+    const aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
+    //Enumeraciones
+    let power;
+    (function (power) {
+        power[power["acuaman"] = 0] = "acuaman";
+        power[power["batman"] = 1] = "batman";
+        power[power["flash"] = 5] = "flash";
+        power[power["superman"] = 100] = "superman";
+    })(power || (power = {}));
+    const fuerzaFlash = power.flash;
+    const fuerzaSuperman = power.superman;
+    const fuerzaBatman = power.batman;
+    const fuerzaAcuaman = power.acuaman;
+    // Retorno de funciones
+    function activar_batiseñal() {
+        return 'activada';
+    }
+    function pedir_ayuda() {
+        console.log('Auxilio!!!');
+    }
+    // Aserciones de Tipo
+    const poder = '100';
+    const largoDelPoder = poder.length;
+    console.log(largoDelPoder);
 })();
---
+++ bases/funciones/args-optional.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    const fullName = (firstName, lastName) => {
+        return `${firstName} ${lastName || 'no lastname'}`;
+    };
+    const name = fullName('Tony', 'Stark');
+    console.log({ name });
+})();
