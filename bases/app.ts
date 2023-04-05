b4173d226b30bec80988b7b85637207000eb6d95
randymz10 <randymz10@gmail.com>
2023-04-05 18:33:45 -0300

    Finished section 4

--- bases/app.ts
+++ bases/app.ts
@@ -1,46 +1,32 @@
-(() => {
+// Funciones Básicas
+function sumar( a: number, b: number ): number{
+  return a + b;
+}
 
-    // Tipos
-    const batman: string = 'Bruce';
-    const superman: string = 'Clark';
-  
-    const existe: boolean = false;
-  
-    // Tuplas
-    const parejaHeroes: [string, string] = [batman,superman];
-    const villano: [string, number, boolean] = ['Lex Lutor',5,true];
-  
-    // Arreglos
-    const aliados: string[] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
-  
-    //Enumeraciones
-    enum power {
-      acuaman = 0,
-      batman = 1,
-      flash = 5,
-      superman = 100
-    }
+const contar = ( heroes:string[] ): number => {
+  return heroes.length;
+}
+const superHeroes: string[] = ["Flash", "Arrow", "Superman", "Linterna Verde"];
+contar(superHeroes);
 
-    const fuerzaFlash: power = power.flash;
-    const fuerzaSuperman: power = power.superman;
-    const fuerzaBatman: power = power.batman;
-    const fuerzaAcuaman: power  = power.acuaman;
-  
-    // Retorno de funciones
-    function activar_batiseñal():string {
-      return 'activada';
-    }
-  
-    function pedir_ayuda(): void{
-      console.log('Auxilio!!!');
-    }
-  
-    // Aserciones de Tipo
-    const poder: any = '100';
-    const largoDelPoder:number = <number>poder.length;
-    console.log( largoDelPoder );
-  
-  
-  })()
-  
-  
+//Parametros por defecto
+const llamarBatman = ( llamar: boolean = true ):void => {
+  if( llamar ){
+    console.log("Batiseñal activada");
+  }
+}
+
+llamarBatman();
+
+// Rest?
+const unirheroes = ( personas: string[] ): string => {
+  return personas.join(", ");
+}
+
+
+// Tipo funcion
+const noHaceNada = ( numero: number, texto: string, booleano: boolean, arreglo:any[] ): void => {}
+
+// Crear el tipo de funcion que acepte la funcion "noHaceNada"
+let noHaceNadaTampoco: (a:number, b:string, c:boolean, d:any[]) => void;
+noHaceNadaTampoco = noHaceNada
